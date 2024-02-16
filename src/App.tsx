a5e36d308383d45fcaeac1491c5a9327828acab8
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-02-16 14:36:28 -0500

    Add API Key input, design system Table and Form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ApiConfig } from '@iqss/dataverse-client-javascript/dist/core'
 import {DataverseApiAuthMechanism} from "@iqss/dataverse-client-javascript/dist/core/infra/repositories/ApiConfig";
 import {getAllDatasetPreviews} from "@iqss/dataverse-client-javascript";
 import {DatasetTable} from "./DatasetTable.tsx";
-ApiConfig.init('http://localhost:8000/api/v1', DataverseApiAuthMechanism.API_KEY, 'b6517bb1-99f3-48c6-b71d-eb7efe284f2b')
+//ApiConfig.init('http://localhost:8000/api/v1', DataverseApiAuthMechanism.API_KEY, 'b6517bb1-99f3-48c6-b71d-eb7efe284f2b')
 
 
 
--- src/DatasetTable.tsx
+++ src/DatasetTable.tsx
@@ -1,12 +1,28 @@
-import  { useState } from 'react';
+import {ChangeEvent, useState} from 'react';
 import {DatasetPreview, getAllDatasetPreviews} from "@iqss/dataverse-client-javascript";
+// @ts-ignore
 import {Button} from "@iqss/dataverse-design-system";
-
+import {Table}   from "@iqss/dataverse-design-system";
+import {Form} from "@iqss/dataverse-design-system";
+import {ApiConfig} from "@iqss/dataverse-client-javascript/dist/core";
+import {DataverseApiAuthMechanism} from "@iqss/dataverse-client-javascript/dist/core/infra/repositories/ApiConfig";
 // Define the type for your object
 
 
 export function DatasetTable()  {
     const [datasetPreviewList, setDatasetPreviewList] = useState<DatasetPreview[]>([]);
+    const [apiKey, setApiKey] = useState(''); // Assuming you have an apiKey state
+
+    // Function to handle the API Key input changes
+    const handleApiKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const {  value } = event.target
+        console.log('value' + value)
+        setApiKey(value)
+        console.log('apiKey' + apiKey)
+        ApiConfig.init('http://localhost:8000/api/v1', DataverseApiAuthMechanism.API_KEY, value)
+
+    }
+
 
     // Action to populate the table
     const populateTable = () => {
@@ -18,25 +34,42 @@ export function DatasetTable()  {
 
     return (
         <div>
+
+            {!apiKey && (
+               <Form>
+                <Form.Group controlId="basic-form-apikey">
+                <Form.Group.Label>API Key</Form.Group.Label>
+                <Form.Group.Input value={apiKey}
+                                  onChange={handleApiKeyChange}  type="text" placeholder="API Key" />
+                </Form.Group>
+                </Form>
+                )}
+            {apiKey && (
+                <>
             <Button onClick={populateTable}>Populate Table</Button>
-            <table>
+            <Table>
                 <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Value</th>
+                    <th>citation</th>
+                    <th>Create Time</th>
+                    <th>Title</th>
+                    <th>Version State</th>
                 </tr>
                 </thead>
                 <tbody>
                 {datasetPreviewList.map(datasetPreview => (
-                    <tr key={datasetPreview.citation}>
+                    <tr key={datasetPreview.versionId}>
+                        <td>{datasetPreview.citation}</td>
                         <td>{datasetPreview.versionInfo.createTime.toUTCString()}</td>
                         <td>{datasetPreview.title}</td>
                         <td>{datasetPreview.versionInfo.state}</td>
                     </tr>
                 ))}
                 </tbody>
-            </table>
+            </Table>
+            </>
+
+)}
         </div>
     )
 }
